f89a54801db14766f1765072c7e1b4377305cd3d
Ansh Kumar <anshkumar1399@gmail.com>
2025-07-02 19:13:09 +0530

    Login User

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,8 @@
 import handleAsyncError from "../middleware/handleAsyncError.js";
 import User from "../models/userModel.js";
+import HandleError from "../utils/handleError.js";
+
+// Register a new user
 
 export const registerUser = handleAsyncError(async (req, res, next) => {
     const { name, email, password } = req.body;
@@ -20,4 +23,27 @@ export const registerUser = handleAsyncError(async (req, res, next) => {
         user,
         token
     });
+})
+
+
+// Login a user
+
+export const loginUser = handleAsyncError(async (req, res, next) => {
+    const { email, password } = req.body;
+    // check if email and password are provided
+    if (!email || !password) {
+        return next(new HandleError("Please provide email and password", 400));
+    }
+    // find user by email
+    const user = await User.findOne({ email }).select("+password");
+    // check if user exists
+    if (!user) {
+        return next(new HandleError("Invalid email or password", 401));
+    }
+    const token = user.getJWTToken();
+    res.status(200).json({
+        success: true,
+        user,
+        token
+    })
 })
