e852cf9e096c080dec6d59283df225ad6f5e067a
Ansh Kumar <anshkumar1399@gmail.com>
2025-07-02 17:25:31 +0530

    Register User Functionality

---
+++ backend/controller/userController.js
@@ -0,0 +1,20 @@
+import handleAsyncError from "../middleware/handleAsyncError.js";
+import User from "../models/userModel.js";
+
+export const registerUser = handleAsyncError(async (req, res, next) => {
+    const { name, email, password } = req.body;
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar: {
+            public_id: "this is temp id",
+            url: "this is temp url"
+        }
+    });
+    res.status(201).json({
+        success: true,
+        user
+    });
+})
