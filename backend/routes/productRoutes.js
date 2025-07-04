21cc74bc0a672e7c724b757776b6e8f233c82a48
Ansh Kumar <anshkumar1399@gmail.com>
2025-07-04 17:32:02 +0530

    Role Based Access

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createProducts, deleteProduct, getAllProducts, getSingleProduct, updateProduct } from '../controller/productController.js';
-import { verifyUserAuth } from '../middleware/userAuth.js';
+import { roleBasedAccess, verifyUserAuth } from '../middleware/userAuth.js';
 
 const router = express.Router();
 
@@ -9,12 +9,12 @@ const router = express.Router();
 // router.route("/api/v1/products").get(getAllProducts);  // it will not work because we give the starting path in app.js we have to give last path
 router.route("/products")
 .get(verifyUserAuth, getAllProducts)
-.post(createProducts);
+.post(verifyUserAuth, roleBasedAccess("admin"), createProducts);
 
 
 router.route("/product/:id")
-.put(updateProduct)
-.delete(deleteProduct)
-.get(getSingleProduct);
+.put(verifyUserAuth, roleBasedAccess("admin"), updateProduct)
+.delete(verifyUserAuth, roleBasedAccess("admin"), deleteProduct)
+.get(verifyUserAuth, getSingleProduct);
 
 export default router;
