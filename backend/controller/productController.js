1203b6205e713ff7ec56023257fb5bc943565706
Ansh Kumar <anshkumar1399@gmail.com>
2025-07-04 17:45:32 +0530

    refer user id in product model

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -7,6 +7,8 @@ import HandleError from "../utils/handleError.js";
 
 //1️⃣ Create Product
 export const createProducts = handleAsyncError(async (req, res, next) => {
+    req.body.user = req.user.id;
+
     const product = await Product.create(req.body)
     res.status(201).json({
         success: true,
