3f02828196a408a363ba9706a881d977067815e7
Ansh Kumar <anshkumar1399@gmail.com>
2025-07-25 15:39:39 +0530

    Reset password using token

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,5 @@
 import handleAsyncError from "../middleware/handleAsyncError.js";
+import crypto from "crypto";
 import User from "../models/userModel.js";
 import HandleError from "../utils/handleError.js";
 import { sendToken } from "../utils/jwtToken.js";
@@ -60,7 +61,7 @@ export const logout = handleAsyncError(async (req, res, next) => {
 })
 
 
-// Reset password
+// Forgot password
 export const requestPasswordReset = handleAsyncError(async (req, res, next) => {
     const { email } = req.body;
     const user = await User.findOne({ email });
@@ -97,3 +98,25 @@ export const requestPasswordReset = handleAsyncError(async (req, res, next) => {
         return next(new HandleError("Could not send reset password email, please try again later", 500));
     }
 });
+
+
+// Reset password
+export const resetPassword = handleAsyncError(async (req, res, next) => {
+    const resetPasswordToken = crypto.createHash("sha256").update(req.params.token).digest("hex");
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() } //check if token is not expired
+    })
+    if (!user) {
+        return next(new HandleError("Reset password token is invalid or has expired", 400));
+    }
+    const { password, confirmPassword } = req.body;
+    if (password !== confirmPassword) {
+        return next(new HandleError("Passwords do not match", 400));
+    }
+    user.password = password; //update password
+    user.resetPasswordToken = undefined; //clear reset token
+    user.resetPasswordExpire = undefined; //clear reset token expiry
+    await user.save(); //save user with new password
+    sendToken(user, 200, res); //send token to user
+})
